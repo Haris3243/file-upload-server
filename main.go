7370ac126c34e1046b5080624585707a1ae2c179
Muhammad Haris Shafiq <harisshafiq08@gmail.com>
2020-01-26 18:09:29 +0500

    file server in golang

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import(
+	"fmt"
+	"os"
+	"io"
+	"http"
+)
+
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+
+    //upload size
+    err := r.ParseMultipartForm(200000) // grab the multipart form
+    if err != nil {
+        fmt.Fprintln(w, err)
+    }
+    
+    //reading original file
+    file, handler, err := r.FormFile("originalFile")
+    if err != nil {
+        fmt.Println("Error Retrieving the File")
+        fmt.Println(err)
+        return
+    }
+    defer file.Close()
+    
+	resFile, err := os.Create("./data/"+handler.Filename)
+	if err != nill {
+		fmt.Fprintln(w, err)
+	}
+    defer resFile.Close()
+    
+    io.Copy(resFile,file)
+    defer resFile.Close()
+    fmt.Fprintf(w, "Successfully Uploaded Original File\n")
+}
+//start server which will listen and server on pot 8083
+func startServer() {
+
+    http.HandleFunc("/uploadFile", uploadFile)
+    
+    //server is listening on port 8083
+    http.ListenAndServe(":8083", nil)
+}
+
+func main() {
+    
+	fmt.Println("Hello World!")
+	startServer()
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//to start file-upload server that will listen and serve on 8083
+func setupRoutes() {
+
+    http.HandleFunc("/fileupload", uploadFile )
+    
+    //server is listening on port 8083
+    http.ListenAndServe(":8083", nil)
+}
